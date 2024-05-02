1b372f5017328798c36fe64490339f773e016713
JLSoaresRamos <jlramossoares@outlook.com.br>
2024-05-02 10:40:07 -0300

    Validations with mongoose

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -49,32 +49,28 @@ app.delete("/api/notes/:id", (request, response) => {
 		.catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
-	if (!request.body.content) {
-		return response.status(400).json({
-			error: "content missing",
-		});
-	}
-
+app.post("/api/notes", (request, response, next) => {
 	const note = new Note({
 		content: request.body.content,
 		important: Boolean(request.body.important) || false,
 	});
 
-	note.save().then((savedNote) => {
-		response.json(savedNote);
-	});
+	note
+		.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response) => {
-	const body = request.body;
+	const { content, important } = request.body;
 
-	const note = {
-		content: body.content,
-		important: body.important,
-	};
-
-	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+	Note.findByIdAndUpdate(
+		request.params.id,
+		{ content, important },
+		{ new: true, runValidators: true, context: "query" }
+	)
 		.then((updatedNote) => {
 			response.json(updatedNote);
 		})
@@ -92,8 +88,9 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === "CastError") {
 		return response.status(400).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
 	}
-
 	next(error);
 };
 
