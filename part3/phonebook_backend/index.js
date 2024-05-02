fccfa820f605f3ff42cdc8c1b415bb8c28145433
JLSoaresRamos <jlramossoares@outlook.com.br>
2024-05-02 10:13:04 -0300

    Deploy to Render

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -16,19 +16,33 @@ morgan.token("body", function (req, res) {
 	return JSON.stringify(req.body);
 });
 
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 
 app.get("/", (request, response) => {
 	response.send("<h1>Welcome to persons phonebook API</h1>");
 });
 
-app.get("/api/persons/", (request, response) => {
-	Person.find({}).then((persons) => {
-		response.json(persons);
-	});
+app.get("/api/persons/", (request, response, error) => {
+	Person.find({})
+		.then((persons) => {
+			response.json(persons);
+		})
+		.catch((error) => next(error));
 });
 
-app.get("/api/info/", (request, response) => {
+app.get("/api/info/", (request, response, next) => {
 	Person.find({})
 		.then((persons) => {
 			response.send(`
@@ -36,22 +50,36 @@ app.get("/api/info/", (request, response) => {
 		<p>${new Date()}</p>
 		`);
 		})
-		.catch((error) => {
-			console.log(error);
-			response.send("<p>Something gone wrong please try later...</p>");
-		});
+		.catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-	Person.findById(request.params.id).then((person) => {
-		response.json(person);
-	});
+	Person.findById(request.params.id)
+		.then((person) => {
+			response.json(person);
+		})
+		.catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-	Person.findByIdAndDelete(request.params.id).then(() => {
-		response.status(202).end();
-	});
+	Person.findByIdAndDelete(request.params.id)
+		.then(() => {
+			response.status(202).end();
+		})
+		.catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+	const body = request.body;
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then((updatedPerson) => response.json(updatedPerson))
+		.catch((error) => next(error));
 });
 
 app.post("/api/persons/", (request, response) => {
@@ -85,9 +113,7 @@ app.post("/api/persons/", (request, response) => {
 					.end();
 			}
 		})
-		.catch((error) => {
-			response.json({ message: "Could not acess persons" });
-		});
+		.catch((error) => next(error));
 });
 
 app.listen(PORT, () => {
